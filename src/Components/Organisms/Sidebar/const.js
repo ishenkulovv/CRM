71b7e963bae878cd9064fd92ab26c1600e9a3942
nurmuhammetjs13 <nurmuhammedaltybaev@gmail.com>
2024-05-09 15:01:20 +0600

    finished HW

--- src/Components/Organisms/Sidebar/const.js
+++ src/Components/Organisms/Sidebar/const.js
@@ -7,4 +7,20 @@ export const MAIN_LIST = [
     id: 'orders',
     label: 'Orders'
   },
+  {
+    id: 'products',
+    label: 'Products'
+  },
+  {
+    id: 'customers',
+    label: 'Customers'
+  },
+  {
+    id: 'analytics',
+    label: 'Analytics'
+  },
+  {
+    id: 'marketing',
+    label: 'Marketing'
+  },
 ]
--- src/Components/Pages/index.js
+++ src/Components/Pages/index.js
@@ -1,7 +1,8 @@
 import Dashboard from "./Dashboard/Dashboard";
 import Orders from "./Orders/Orders";
+import Analytics from "./Analytics/Analytics";
+import Marketing from "./Marketing/Marketing";
+import Products from "./Products/Products";
+import Customers from "./Customers/Customers";
 
-export {
-  Dashboard,
-  Orders
-}
+export { Dashboard, Orders, Analytics, Marketing, Products, Customers };
