3c515d88569bc2413628603c41b09bbf6739e641
Adil Ishenkulov <adil.ishenkulov@axelkg.com>
2024-06-04 21:09:03 +0600

    changes

--- src/Components/Pages/Orders/Orders.jsx
+++ src/Components/Pages/Orders/Orders.jsx
@@ -1,32 +1,35 @@
 import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux';
+
 import MainTemplate from '../../Templates/MainTemplate/MainTemplate';
-import { BlockContainer } from '../../../Base/styled';
+
+import RemoveModal from '../../Organisms/RemoveModal/RemoveModal';
+
 import Tabs from '../../Molecules/Tabs/Tabs';
-import { TABS_LIST, ordersList, PAYMENT_OPTIONS } from './const';
 import Table from '../../Molecules/Table/Table';
+
 import Search from '../../Atoms/Search/Search';
 import CustomDatePicker from '../../Atoms/CustomDatePicker/CustomDatePicker';
 import FilterButton from '../../Atoms/FilterButton/FilterButton';
 import Select from '../../Atoms/Select/Select';
-import { FlexRow } from './styled';
 import Button from '../../Atoms/Button/Button';
-import ModalTemplate from '../../Templates/ModalTemplate/ModalTemplate';
-import RemoveModal from '../../Organisms/RemoveModal/RemoveModal';
-import { useDispatch, useSelector } from 'react-redux';
-import { checkedForRemove, removeHandler, searchHandler } from '../../../Store/Slice/ordersSlice';
+
+import { checkedForRemove, removeHandler, searchHandler, searchPaymentHandler, searchStatusHandler, setData, tabHandler, visibleRemoveModal } from '../../../Store/Slice/ordersSlice';
+
+import { FlexRow } from './styled';
+import { BlockContainer } from '../../../Base/styled';
+
+import { ordersList, PAYMENT_OPTIONS } from './const';
+
 
 export default function Orders() {
-
   const dispatch = useDispatch()
 
-  const [tabsList, setTabsList] = useState(TABS_LIST);
   const [search, setSearch] = useState('');
-  // const [data, setData] = useState(ordersList?.map(item => ({...item, checked: false})))
   const data = useSelector(state => state?.rootReducer?.ordersSlice?.data)
-  console.log(data);
+  const visibleRemove = useSelector(state => state?.rootReducer?.ordersSlice?.remove_modal_visible);
+  const tabsList = useSelector(state => state?.rootReducer?.ordersSlice?.tabs_list);
   const [filtersShow, setFiltersShow] = useState(false)
-  const [currentPayment, setCurrentPayment] = useState(null)
-  const [visibleRemove, setVisibleRemove] = useState(false)
 
   useEffect(() => {
     searchByTitle(search.trim())
@@ -34,51 +37,24 @@ export default function Orders() {
 
 
   const setTab = (id) => {
-    const data = tabsList.map(tab => {
-      if (tab.id === id) {
-        tab.active = true;
-      } else {
-        tab.active = false;
-      }
-      return tab;
-    })
-    setTabsList(data);
+    dispatch(tabHandler(id))
     searchByStatus(id)
   }
 
-  const searchByStatus = (id) => {
-    const sendData = ordersList.filter(item => {
-      return id === 'all' ? item : item.status.toLowerCase() === id.toLowerCase()
-    })
-    setData(sendData)
-  }
+  const searchByStatus = (id) => dispatch(searchStatusHandler(id))
 
   const searchByTitle = (title) => dispatch(searchHandler(title))
 
-  const searchByPrice = (e) => {
-    const value = e.target.value;
-    const sendData = ordersList.filter(item => {
-      return value === 'none' ? item : item.price === value
-    })
-    setData(sendData)
-  }
-
-  const searchByPayment = (payment) => {
-    const value = payment?.value
-
-    const sendData = ordersList.filter(item => {
-      if (!value) return item;
-      return item?.payment_status?.toLowerCase()?.trim() === value?.toLowerCase()?.trim()
-    })
-    setData(sendData)
-  }
+  const searchByPayment = (payment) => dispatch(searchPaymentHandler(payment))
 
   const checkedHandler = (id, status) => dispatch(checkedForRemove({id, status}));
 
   const removeOrder = () => dispatch(removeHandler())
 
-  const showRemove = () => setVisibleRemove(true);
-  const hideRemove = () => setVisibleRemove(false);
+  const searchByDate = (results) => dispatch(setData(results))
+
+  const showRemove = () => dispatch(visibleRemoveModal(true))
+  const hideRemove = () => dispatch(visibleRemoveModal(false))
 
   return (
     <MainTemplate title="Orders">
@@ -87,17 +63,11 @@ export default function Orders() {
         <Tabs list={tabsList} setTab={setTab} />
         <FlexRow>
           <Search value={search} setValue={setSearch} />
-          {/* <CustomDatePicker data={ordersList} setData={setData} /> */}
+          <CustomDatePicker data={ordersList} setData={searchByDate} />
           <FilterButton active={filtersShow} setFilters={setFiltersShow} />
           <Button handler={showRemove}>Remove order</Button>
         </FlexRow>
-        {filtersShow ? 
-          (
-            <Select options={PAYMENT_OPTIONS} handler={searchByPayment} />
-          )
-          :
-          null
-        }
+        {filtersShow ? <Select options={PAYMENT_OPTIONS} handler={searchByPayment} /> : null}
         <Table data={data} handlerCheckbox={checkedHandler} />
       </BlockContainer>
     </MainTemplate>
--- src/Store/Slice/ordersSlice.js
+++ src/Store/Slice/ordersSlice.js
@@ -1,9 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { ordersList } from "../../Components/Pages/Orders/const";
+import { TABS_LIST, ordersList } from "../../Components/Pages/Orders/const";
 
 const initialState = {
   data: ordersList?.map(item => ({...item, checked: false})),
+  tabs_list: TABS_LIST,
   remove_count: false,
+  remove_modal_visible: false,
 };
 
 const ordersSlice = createSlice({
@@ -11,28 +13,49 @@ const ordersSlice = createSlice({
   initialState,
   reducers: {
     checkedForRemove(state, action) {
-      state.data = state.data.map(item => {
-        if (item.id === action.payload?.id) {
-          item.checked = action.payload?.status
-        }
-        return item
-      })
+      state.data = state.data?.map(item => item?.id === action?.payload?.id ? {...item, checked: true} : item);
     },
     removeHandler(state, action) {
       const s = state.data.filter(item => !item.checked)
       state.remove_count = s.length === state.data.length ? true : false
       if (state.remove_count) {
         alert('Ордер танданыз!')
+        state.remove_modal_visible = false;
         return;
       }
       state.data = s;
+      state.remove_modal_visible = false;
     },
     searchHandler(state, action) {
       state.data = ordersList.filter(item => item.title.toLowerCase().trim().includes(action.payload.toLowerCase().trim()))
+    },
+    searchStatusHandler(state, action) {
+      state.data = ordersList.filter(item => action.payload === 'all' ? item : item.status.toLowerCase() === action.payload.toLowerCase())
+    },
+    searchPaymentHandler(state, action) {
+      state.data = ordersList.filter(item => !action.payload?.value ? item : item?.payment_status?.toLowerCase()?.trim() === action.payload?.value?.toLowerCase()?.trim())
+    },
+    visibleRemoveModal(state, action) {
+      state.remove_modal_visible = action.payload;
+    },
+    tabHandler(state, action) {
+      state.tabs_list = state.tabs_list?.map(tab => tab?.id === action.payload ? {...tab, active: true } : {...tab, active: false })
+    },
+    setData(state, action) {
+      state.data = action.payload;
     }
   }
 })
 
-export const {checkedForRemove, removeHandler, searchHandler} = ordersSlice.actions
+export const {
+  checkedForRemove, 
+  removeHandler, 
+  searchHandler, 
+  searchStatusHandler, 
+  searchPaymentHandler, 
+  visibleRemoveModal, 
+  tabHandler,
+  setData
+} = ordersSlice.actions
 
 export default ordersSlice.reducer
